d4649e436497796dfdf77cd90e501adc70e333bb
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-01-16 14:55:14 +0200

    Created Script + Build of Project

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,17 +4,43 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 
-document.querySelector('.form-header__search').nextElementSibling.parentNode.removeChild(document.querySelector('.form-header__search').nextSibling)
+const button = document.querySelector('.form-header__button');
+const input = document.querySelector('.form-header__search');
+const ip = document.querySelector('#ip');
+const location = document.querySelector('#lc');
+const timeZone = document.querySelector('#tz');
+const itp = document.querySelector('#itp');
+let mapScale = 5;
+let map = L.map('map').setView([51.505, -0.09], mapScale);
 
-let map = L.map('map').setView([51.505, -0.09], 5);
+async function findLocation() {
+    let data = await fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_VHgKvKYZy0a6NX350OVL7lj82IZJF&ipAddress=' + input.value)
+    let response = await data.json();
+    ip.textContent = response.ip;
+    location.textContent = response.location.country + ', ' + response.location.city;
+    timeZone.textContent = response.location.timezone;
+    itp.textContent = response.isp;
+    editMap([response.location.lat, response.location.lng])
+}
+const editMap = (lg) => {
+    mapScale = 17;
+    L.marker(lg).addTo(map)
+        .bindPopup('Your Location')
+        .openPopup();
+}
+input.nextElementSibling.parentNode.removeChild(input.nextSibling) // Remove Whitespace
+button.addEventListener("click", (callback) => {
+    findLocation()
+    callback.preventDefault()
+});
 map.touchZoom.disable();
 map.doubleClickZoom.disable();
-map.scrollWheelZoom.disable();
+// map.scrollWheelZoom.disable();
 map.boxZoom.disable();
 map.keyboard.disable();
-let mapIcon = L.icon({
-    iconUrl: '../../img/images/icon-location.svg'
-})
+// let mapIcon = L.icon({
+//     iconUrl: '../../img/images/icon-location.svg'
+// })
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -27,7 +53,3 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     boxZoom: false,
     accessToken: 'pk.eyJ1IjoiaW51bWFraWUiLCJhIjoiY2t3d290amZiMDVyNzJvbGFpOWI4bDE4NiJ9.sCoH7SuL3X_ohxx1R-kRRw'
 }).addTo(map);
-
-L.marker([51.5, -0.09], { icon: mapIcon }).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
